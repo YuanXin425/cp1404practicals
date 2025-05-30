b55309dafbb29fb15e1c26e406c207ff087303da
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-05-31 02:00:48 +0800

    Use the standard menu pattern to adjust the main menu function. Add get_score, print_result and show_stars functions.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,10 +1,47 @@
+from prac_02.score import get_score
+
+
 def main():
-    score = int(input("Enter score: "))
-    while score != "Quit":
-        if score == 0 - 100:
-            print("*")
+    print("Menu:")
+    print("(G)et a valid score(must be 0-100 inclusive)")
+    print("(P)rint result")
+    print("(S)how stars")
+    print("(Q)uit")
+    choice = input("Enter choice: ")
+    while choice != "Quit":
+        if choice == "G":
+            score = get_score()
+        elif choice == "P":
+            print_result(score)
+        elif choice == "S":
+            show_stars(score)
+        elif choice == "Q":
+            print("Farewell")
+
         else:
-            print("Invalid score")
+            print("Invalid choice. Please reselect again.")
 
     print("Farewell")
+
+def get_score():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+
+    print(score)
+
+def print_result(score):
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+def show_stars(score):
+    print("*" * int(round(score)))
+
+
+
 
