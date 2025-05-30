bf9e8ae2e29d4fea6b5fa7987fc6642fc2f1e9eb
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-05-31 00:35:53 +0800

    Create the Menu

---
+++ prac_02/score_menu.py
@@ -0,0 +1,10 @@
+def main():
+    score = int(input("Enter score: "))
+    while score != "Quit":
+        if score == 0 - 100:
+            print("*")
+        else:
+            print("Invalid score")
+
+    print("Farewell")
+
