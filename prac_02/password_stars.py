4ddc39c1052ce369c31aba4d344e898b2863b8e2
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-05-31 23:54:24 +0800

    Replace print_asterisks to print_stars. Change input("Enter your password: ") to input("Enter password: ").

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,17 +1,17 @@
 def main():
     password = get_password()
-    print_asterisks(password)
+    print_stars(password)
 
 def get_password():
     """Get a valid password from the user."""
-    password = input("Enter your password: ")
+    password = input("Enter password: ")
     while password == "":
         print("Password cannot be blank.")
-        password = input("Enter your password: ")
+        password = input("Enter password: ")
     return password
 
-def print_asterisks(password):
-    """Print asterisks equal to the length of the password."""
+def print_stars(password):
+    """Print stars equal to the length of the password."""
     print("*" * len(password))
 
 main()
