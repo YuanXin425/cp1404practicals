911c938027ce983e5424b50743202ced2ac59689
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-05-31 23:36:01 +0800

    Add comments.

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,9 +1,13 @@
 def main():
+    # Get user's input
     celsius = float(input("Celsius: "))
+    # Use the function to convert
     fahrenheit = celsius_to_fahrenheit(celsius)
     print(f"Result: {fahrenheit:.2f} F")
 
+    # Get user's input
     fahrenheit = float(input("Fahrenheit: "))
+    # Use the function to convert
     celsius = fahrenheit_to_celsius(fahrenheit)
     print(f"Result: {celsius:.2f} C")
 
