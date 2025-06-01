621592c9b8fcd393f0ef1cabeac8e3c076f49b84
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-01 21:23:26 +0800

    Add MENU global constant. Add print(MENU), choice = input(">>> ").upper(), while choice != "Q":, if choice == "C":, elif choice == "F":, else: print("Invalid option"), and print("Thank you.") under main function. Add a docstring above print(MENU).

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,15 +1,33 @@
-def main():
-    # Get user's input
-    celsius = float(input("Celsius: "))
-    # Use the function to convert
-    fahrenheit = celsius_to_fahrenheit(celsius)
-    print(f"Result: {fahrenheit:.2f} F")
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
 
-    # Get user's input
-    fahrenheit = float(input("Fahrenheit: "))
-    # Use the function to convert
-    celsius = fahrenheit_to_celsius(fahrenheit)
-    print(f"Result: {celsius:.2f} C")
+def main():
+    """Get user's input. Determine user's input. Print temperature conversion result."""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            # Get user's input
+            celsius = float(input("Celsius: "))
+            # Use the function to convert
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+
+        elif choice == "F":
+            # Get user's input
+            fahrenheit = float(input("Fahrenheit: "))
+            # Use the function to convert
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+
+        else:
+            print("Invalid option")
+
+        print(MENU)
+        choice = input(">>> ").upper()
+
+    print("Thank you.")
 
 def celsius_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit"""
