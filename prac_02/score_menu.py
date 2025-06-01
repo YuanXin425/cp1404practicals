69ffcaf4c360500fa8ba51c9173ab883c77ab137
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-01 21:38:42 +0800

    Remove if score < 0 or score > 100: print("Invalid score") and change elif score >= 90: to if score >= 90: under print_result(score) function.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -38,9 +38,7 @@ def check_score():
 
 def print_result(score):
     """Determine result based on the score."""
-    if score < 0 or score > 100:
-        print("Invalid score")
-    elif score >= 90:
+    if score >= 90:
         print("Excellent")
     elif score >= 50:
         print("Passable")
