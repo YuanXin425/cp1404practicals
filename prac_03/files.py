3471f4b5613f4a136fe4e117be9be9f6ea218d50
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-07 03:06:44 +0800

    1. Separate the codes individually for each of the questions in comments. 2. Add total = 0 and with open("numbers.txt", "r") as numbers_file: to the codes for the 4th question. 3. Add numbers_file.close() to the codes for the 4th question. 4. Add name_file = open('name.txt'), name = input("Enter name: "), name_file.readline().strip(), print("Hi", name, "!"), and name_file.close() to the codes for the 2nd question.

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,29 +1,43 @@
+# 1.
 # Open a file called name.txt
 name_file = open('name.txt', 'w')
-
 # Get user's name and write it to the file
 name = input("Enter name: ")
 name_file.write(name)
-
 # Close the file
 name_file.close()
 
 
+# 2.
+# Open a file called name.txt
+name_file = open('name.txt')
+# Get user's name and write it to the file
+name = input("Enter name: ")
+name_file.readline().strip()
+print("Hi", name, "!")
+# Close the file
+name_file.close()
+
+
+# 3.
 total = 0
 with open("numbers.txt", "r") as numbers_file:
     # Read the first two numbers and adds them together
     for i in range(2):
         number = numbers_file.readline()
         total += int(number)
-
     print(total)
+# Close the file
+numbers_file.close()
 
+
+# 4.
+total = 0
+with open("numbers.txt", "r") as numbers_file:
     # Read all the numbers and adds them together
     for line in numbers_file:
         total = total + int(line)
-
     print(total)
-
 # Close the file
 numbers_file.close()
 
