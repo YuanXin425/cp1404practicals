5d00fb0ed13c24298897008e1effcfed070d9c6f
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-07 02:32:07 +0800

    1. Answers to the questions in comments. 2. Create print(random.randint(1,100)).

---
+++ prac_03/randoms.py
@@ -0,0 +1,20 @@
+# What did you see on line 1?
+# I see 6 on line 1.
+# What was the smallest number you could have seen, what was the largest?
+# I could have seen 5 as the smallest number and 20 for the largest number.
+
+# What did you see on line 2?
+# I see 5 on line 2.
+# What was the smallest number you could have seen, what was the largest?
+# I could have seen 3 as the smallest number and 9 for the largest number.
+# Could line 2 have produced a 4?
+# No, as it is 2 steps, 3 + 2 = 5, hence a 4 could not be produced.
+
+# What did you see on line 3?
+# I see 2.5046370584499535 on line 3.
+# What was the smallest number you could have seen, what was the largest?
+# I could have seen 2.5 as the smallest number and 5.5 for the largest number.
+
+# Generate a random number between number 1 to 100.
+import random
+print(random.randint(1,100))
