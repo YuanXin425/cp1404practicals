3736abe69641caca2f5e850c05652d7e2b9b5cf2
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-09 17:55:05 +0800

    1. In for month in range(1, number_of_months + 1):, change number_of_months to len(incomes) 2. Remove number_of_months = 0 above total = 0.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -17,9 +17,8 @@ def main():
 
 def print_report(incomes):
     print("\nIncome Report\n-------------")
-    number_of_months = 0
     total = 0
-    for month in range(1, number_of_months + 1):
+    for month in range(1, len(incomes) + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
