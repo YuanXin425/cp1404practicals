912ca78a765c009a99530a19a027f839185e6f1d
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-09 23:22:59 +0800

    For Basic list operations: 1. Remove the following codes:     smallest_number = min(numbers)     largest_number = max(numbers) 2. Change number = float(input("Number:")) to number = int(input("Number:"))

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,16 +3,17 @@
 numbers = []
 # Get user's 5 numbers.
 for i in range(5):
-    number = float(input("Number:"))
+    number = int(input("Number:"))
     numbers.append(number) # Add each number to the list.
-    smallest_number = min(numbers) # Find the smallest number.
-    largest_number = max(numbers) # Find the largest number.
+
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[4]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
+
 average = sum(numbers) / 5 # Calculate the average.
 print(f"The average of the numbers is {average}")
+
 
 # 2. Woefully inadequate security checker
 # List of usernames.
