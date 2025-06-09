68cbfe271858a0c3558f790f7530d6b2fe1dede4
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-09 15:32:34 +0800

    For question 1: 1. Write the codes. 2. Write down the values for each expression. 3. Run the codes to check.

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+# 1.
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+print(numbers[0]) # Answer: The value for the expression is 3.
+print(numbers[-1]) # Answer: The value for the expression is 2.
+print(numbers[3]) # Answer: The value for the expression is 4.
+print(numbers[:-1]) # Answer: The values for the expression is [3, 1, 4, 1, 5, 9].
+print(numbers[3:4]) # Answer: The value for the expression is 1.
+print(5 in numbers) # Answer: The value for the expression is [3, 1, 4, 1, 5].
+print(7 in numbers) # Answer: The value for the expression is [3, 1, 4, 1, 5, 9, 2].
+print("3" in numbers) # Answer: The value for the expression is True/3.
+result = numbers + [6, 5, 3]
+print(result) # Answer: The value for the expression is [3, 1, 4, 1, 5, 9, 2, 6, 5, 3].
+
+
+
