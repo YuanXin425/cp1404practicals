12dcdd8c47c602b261643edd0f3e418c7f4c0a08
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-11 19:32:09 +0800

    1.Change "," to ", " in long_names = ",".join(long_names) 2. Change long_names to last_names.

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -47,7 +47,7 @@ print(greater_than)
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
-long_names = [name.split()[-1] for name in full_names if len(name) > 11]
-long_names = ",".join(long_names)
-print(long_names)
+last_names = [name.split()[-1] for name in full_names if len(name) > 11]
+last_names = ", ".join(last_names)
+print(last_names)
 
