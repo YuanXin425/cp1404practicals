33a3b0a06d3ff5027fb013affb1fe1a176a7014b
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-11 22:08:20 +0800

    1. Remove NUMBERS = []
    Under random_numbers(),
    1. Add the following codes:
        numbers = []
        while len(numbers) < 6:
            num = random.randint(1,45)
    
            if num not in numbers:
                numbers.append(num)
    
    2. Change return numbers to return ' '.join(f"{num:2}" for num in numbers).
    3. Add comments.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,7 +1,5 @@
 import random
 
-NUMBERS = []
-
 def main():
     """Get user's number of quick picks."""
     picks = int(input("How many quick picks?"))
@@ -11,9 +9,15 @@ def main():
 
 def random_numbers():
     """Generate random numbers in ascending order."""
-    numbers = [random.randint(1,45) for _ in range(6)]
+    numbers = []
+    while len(numbers) < 6:
+        num = random.randint(1,45)
+        # Ensures no repetition of numbers
+        if num not in numbers:
+            numbers.append(num)
+    # Sorts the numbers in ascending order before returning
     numbers.sort()
-    return numbers
+    return ' '.join(f"{num:2}" for num in numbers)
 
 main()
 
