2faa2140ee93fd32d697d03c14b9a74500730742
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-12 11:30:13 +0800

    Under main function: 1. Add:     for i in range(picks):         print(random_numbers()) 2. Remove the rest of the codes.
    Create random_numbers function.
    Under random_numbers function:
    1. Add the following codes:
        numbers = list(range(MIN_NUMBER,MAX_NUMBER))
        random.shuffle(numbers))
    
        selected_numbers = numbers[:NUMBERS_PER_PICKS]
        selected_numbers.sort()
    
        return ' '.join(f"{num:2}" for num in selected_numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,20 +8,19 @@ NUMBERS_PER_PICKS = 6
 def main():
     """Get user's number of picks."""
     picks = int(input("How many quick picks?"))
-
-    # Generate random numbers between 1(minimum) and 45(maximum)
     for i in range(picks):
-        random_numbers = []
-        for num in range(NUMBERS_PER_PICKS):
-            num = random.randint(MIN_NUMBER, MAX_NUMBER)
-            # Ensures no repetition of numbers per pick
-            while num in random_numbers:
-                num = random.randint(MIN_NUMBER, MAX_NUMBER)
-            random_numbers.append(num)
-        # Sorts the numbers in ascending order
-        random_numbers.sort()
-        # list to string
-        print(' '.join(f"{num:2}" for num in random_numbers))
+        print(random_numbers())
+
+def random_numbers():
+    """Generate random numbers between 1(minimum) and 45(maximum)"""
+    numbers = list(range(MIN_NUMBER,MAX_NUMBER))
+    random.shuffle(numbers)
+    # Ensures no repetition of numbers per pick
+    selected_numbers = numbers[:NUMBERS_PER_PICKS]
+    # Sorts the numbers in ascending order
+    selected_numbers.sort()
+    # list to string
+    return ' '.join(f"{num:2}" for num in selected_numbers)
 
 main()
 
