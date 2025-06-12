feb1c6f8ada3318a21dc60967220bceadb4b59d1
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-12 11:40:55 +0800

    For expression 3, 6, 7 and 8: 1. I change my answers after I fully understand the expressions, and after that I check my answers by using the Python console.

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -3,17 +3,16 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 
 print(numbers[0]) # Answer: The value for the expression is 3.
 print(numbers[-1]) # Answer: The value for the expression is 2.
-print(numbers[3]) # Answer: The value for the expression is 4.
+print(numbers[3]) # Answer: The value for the expression is 1.
 print(numbers[:-1]) # Answer: The values for the expression is [3, 1, 4, 1, 5, 9].
 print(numbers[3:4]) # Answer: The value for the expression is 1.
-print(5 in numbers) # Answer: The value for the expression is [3, 1, 4, 1, 5].
-print(7 in numbers) # Answer: The value for the expression is [3, 1, 4, 1, 5, 9, 2].
-print("3" in numbers) # Answer: The value for the expression is True/3.
+print(5 in numbers) # Answer: The value for the expression is True.
+print(7 in numbers) # Answer: The value for the expression is False.
+print("3" in numbers) # Answer: The value for the expression is False.
 result = numbers + [6, 5, 3]
 print(result) # Answer: The value for the expression is [3, 1, 4, 1, 5, 9, 2, 6, 5, 3].
 
 # 2)
-
 # 1.
 numbers[0] = "ten"
 print(numbers)
