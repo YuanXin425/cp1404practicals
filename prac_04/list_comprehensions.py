d2bac8320a3070860d03d9940dbb29d71b03ce96
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-06-13 22:13:32 +0800

    1. Change greater_than to greater_numbers.

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -41,8 +41,8 @@ print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
-greater_than = [greater_than for greater_than in numbers if greater_than > 9]
-print(greater_than)
+greater_numbers = [greater_numbers for greater_numbers in numbers if greater_numbers > 9]
+print(greater_numbers)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
