d3b9d8685b8094d5aad658b617216ba41e0d68c3
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-07-12 18:09:52 +0800

    1. Copy my guitar.py from prac_06 folder. 2. Add a __lt__(less than) method to compare the Guitars by year.

---
+++ prac_07/guitar.py
@@ -0,0 +1,25 @@
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Print the guitar details."""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def __lt__(self, other):
+        """Compare Guitars by year in order to sort the list by year from the oldest guitar to the newest guitar."""
+        return self.year < other.year
+
+    def get_age(self):
+        """Get the age of the guitar."""
+        current_year = 2022
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Determine if the guitar is vintage."""
+        return self.get_age() >= 50
