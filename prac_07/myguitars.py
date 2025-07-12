bbd0ad02faf8961fb5f060f55c2a92318c4d6061
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-07-12 18:04:13 +0800

    1. Create a program to read all guitar details and store them in a list of Guitar objects.

---
+++ prac_07/myguitars.py
@@ -0,0 +1,17 @@
+from prac_07.guitar import Guitar
+
+def main():
+    """Read file of guitar details and store them in a list of Guitar objects."""
+    guitars = []
+    with open("guitars.csv", "r") as in_file:
+        for line in in_file:
+            parts = line.strip().split(",")
+            guitar = Guitar(parts[0], parts[1], float(parts[2]))
+            guitars.append(guitar)
+
+    in_file.close()
+
+    for guitar in guitars:
+        print(guitar)
+
+main()
