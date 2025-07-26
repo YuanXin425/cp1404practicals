321b58370531773da8ef1f57344364aaf4c4113c
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-07-27 00:06:14 +0800

    1. Add a class word in the docstring.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,7 +1,7 @@
 from silver_service_taxi import SilverServiceTaxi
 
 def main():
-    """Test SilverServiceTaxi."""
+    """Test SilverServiceTaxi class."""
     taxi_1 = SilverServiceTaxi("Hummer", 100, 2)
     taxi_1.drive(18)
     print(taxi_1.get_fare())
