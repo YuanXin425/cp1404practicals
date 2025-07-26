f45ade6cec9accea01e187728ba47faa6c1b1751
YuanXin425 <yuanxin.chong@my.jcu.edu.au>
2025-07-27 00:08:05 +0800

    1. Add a class word in the docstring.

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,7 +1,7 @@
 from prac_09.unreliable_car import UnreliableCar
 
 def main():
-    """Test UnreliableCar."""
+    """Test UnreliableCar class."""
     # Create unreliable car objects
     my_unreliable_car = UnreliableCar("Unreliable Car", 100, 10)
     my_reliable_car = UnreliableCar("Reliable Car", 100, 90)
